d412bc65869ce67b0265fe52b7d74f202b8ff64d
Viktoria Rosovska <torimeister@gmail.com>
2025-05-10 22:52:00 +0300

    feat: add 7 test

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,52 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi } from '07-mocking-lib-api';
 
+jest.mock('axios');
+const baseURL = 'https://jsonplaceholder.typicode.com';
+const mockData = {
+  data: {
+    name: 'Username',
+    age: 42,
+  },
+};
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const axiosCreateMock = jest.spyOn(axios, 'create').mockReturnValue({
+      get: jest.fn().mockResolvedValue({ data: mockData }),
+    } as unknown as AxiosInstance);
+    await throttledGetDataFromApi('/user/1');
+    expect(axios.create).toHaveBeenCalledWith({ baseURL });
+    axiosCreateMock.mockReset();
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const axiosCreateMock = jest.spyOn(axios, 'create').mockReturnValue({
+      get: jest.fn().mockResolvedValue({ data: mockData }),
+    } as unknown as AxiosInstance);
+    await throttledGetDataFromApi('/user/1');
+    jest.runAllTimers();
+    const instance = axios.create({ baseURL });
+    expect(instance.get).toHaveBeenCalledWith('/user/1');
+    axiosCreateMock.mockReset();
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const axiosCreateMock = jest.spyOn(axios, 'create').mockReturnValue({
+      get: jest.fn().mockResolvedValue({ data: mockData }),
+    } as unknown as AxiosInstance);
+    const result = await throttledGetDataFromApi('/user/1');
+    jest.runAllTimers();
+    expect(result).toBe(mockData);
+    axiosCreateMock.mockReset();
   });
 });
